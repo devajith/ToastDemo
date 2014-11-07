73f2bcdde05c012b701b5a649be0a00ebed31b27
devajith <dev.ajith89@gmail.com>
2014-11-07 20:45:02 +0530

    Initial Commit

---
+++ ToastDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToastDemo
+//
+//  Created by Ajith Kumar on 07/11/14.
+//  Copyright (c) 2014 Ajith Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToastDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToastDemo
+//
+//  Created by Ajith Kumar on 07/11/14.
+//  Copyright (c) 2014 Ajith Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
